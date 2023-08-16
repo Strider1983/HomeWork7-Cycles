b2b80a900dc9c23cdf2269bb9a56ba530ef72109
Strider <mezentsev.k@gmail.com>
2023-08-16 19:51:27 +0800

    Задача №1 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Циклы. Урок 2");
+        System.out.println("___________________");
+        System.out.println("Задача №1");
+        int salary = 15000;
+        int total = 0;
+        int month = 0;
+        while (total < 2_459_000) {
+            total = total + salary;
+            month = month + 1;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + total + " рублей");
+        }
     }
 }
